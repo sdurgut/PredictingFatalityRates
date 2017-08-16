babb5e2c0de2d3b045b31ac5d3f367354e1e02e8
Suleyman Durgut <suleyman-durgut@uiowa.edu>
2017-08-16 12:21:55 -0500

    update

---
+++ README.md
@@ -0,0 +1,6 @@
+# Predicting the Fatality Rates from Motor Vehicle Crashes in U.S. at County Level
+
+This package uses the fatality data provided by NHTSA and population data provided by National Cancer Institute. The package analyzes the data from 1976 to 2011, and predicts the fatality rates for 2011-2015. 
+
+CrateSVG file takes the prediction and actual data for fatality rates and put them on a U.S. map.
+ 
---
+++ src/color_map_AdjustedPopulation.py
@@ -0,0 +1,67 @@
+import csv
+from BeautifulSoup import BeautifulSoup
+
+data = {}
+reader = csv.reader(open('FatalitybyCountyAdjustedwithPopulation.csv'), delimiter=",")
+for row in reader:
+	tmp = ''
+	if len(row[0])<5:
+		tmp = "0"+row[0]
+	else: tmp = row[0]
+	data[tmp] = float(row[3].strip())
+
+
+# Load the SVG map
+svg = open('USA_Counties_with_FIPS_and_names.svg', 'r').read()
+
+# Load into Beautiful Soup
+soup = BeautifulSoup(svg, selfClosingTags=['defs','sodipodi:namedview'])
+# Find counties
+paths = soup.findAll('path')
+
+# Map colors
+colors = ["#f7f4f9", "#e7e1ef", "#d4b9da", "#c994c7", "#df65b0", "#e7298a", "#ce1256", "#980043", "#67001f"]
+
+
+# County style
+path_style = 'font-size:12px;fill-rule:nonzero;stroke:#FFFFFF;stroke-opacity:1;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-linecap:butt;marker-start:none;stroke-linejoin:bevel;fill:'
+
+
+
+# Color the counties based on unemployment rate
+for p in paths:
+	 
+	if p['id'] not in ["State_Lines", "separator"]:
+		# pass
+		try:
+			rate = data[p['id']]
+		except:
+			continue
+			 
+		if rate > 40:
+			color_class = 8
+		elif rate > 35:
+			color_class = 7
+		elif rate > 30:
+			color_class = 6
+		elif rate > 25:
+			color_class = 5
+		elif rate > 20:
+			color_class = 4
+		elif rate > 15:
+			color_class = 3
+		elif rate > 10:
+			color_class = 2
+		elif rate > 5:
+			color_class = 1
+		else:
+			color_class = 0
+ 
+		color = colors[color_class]
+		p['style'] = path_style + color
+
+
+
+# Output map
+print soup.prettify()
+
